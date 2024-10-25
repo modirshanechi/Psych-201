22fbfd79421e443240ea1f4e3fad20f365490604
marcelbinz <marcelbinz1@gmail.com>
2024-10-25 13:05:20 +0200

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def randomized_choice_options(num_choices):
+    choice_options = list(map(chr, range(65, 91)))
+    return np.random.choice(choice_options, num_choices, replace=False)
