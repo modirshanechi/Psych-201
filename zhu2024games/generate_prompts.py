23bd383b5b1e0a60cf42553f26488497ab0b698b
jqzhu <jianqiao.zhu@warwick.ac.uk>
2025-03-11 22:00:06 -0400

    update after review

--- zhu2024games/generate_prompts.py
+++ zhu2024games/generate_prompts.py
@@ -19,6 +19,7 @@ def permutate_game_matrix(M, row, col):
 all_prompts = []
 for pId in pIds: # loop through all participants
     df = df_ind[df_ind.participant_id == pId]
+    prompt = ''
     for tId in range(1,21,1): # loop through all 20 games this paprticipant have played
         M = df[df.trial_id == tId].row_form_matrix.values[0] # game matrix
         row_switch = df[df.trial_id == tId].row_switch.values[0]
@@ -29,16 +30,16 @@ for pId in pIds: # loop through all participants
         if row_switch == 1: 
             up_choice = 1-up_choice # rowA=1, rowB=0
         
-        prompt = "You've been matched with a new player for a game. You are designated as the 'row player'.\n" +\
-        "You can choose between two options: Row A and Row B. Your opponent will simultaneously choose between Column C and Column D.\n" +\
-        "Your payoff, and your opponent's payoff, depend on the combination of choices:\n" +\
-        f"If you choose Row A and your opponent chooses Column C, you receive {M_on_screen[0]} and your opponent receives {M_on_screen[4]}.\n" +\
-        f"If you choose Row A and your opponent chooses Column D, you receive {M_on_screen[1]} and your opponent receives {M_on_screen[5]}.\n" +\
-        f"If you choose Row B and your opponent chooses Column C, you receive {M_on_screen[2]} and your opponent receives {M_on_screen[6]}.\n" +\
-        f"If you choose Row B and your opponent chooses Column D, you receive {M_on_screen[3]} and your opponent receives {M_on_screen[7]}.\n" +\
-        f"You press <<{'Row A' if up_choice==1 else 'Row B'}>>."
+    prompt += "You've been matched with a new player for a game. You are designated as the 'row player'.\n" +\
+    "You can choose between two options: Row A and Row B. Your opponent will simultaneously choose between Column C and Column D.\n" +\
+    "Your payoff, and your opponent's payoff, depend on the combination of choices:\n" +\
+    f"If you choose Row A and your opponent chooses Column C, you receive {M_on_screen[0]} and your opponent receives {M_on_screen[4]}.\n" +\
+    f"If you choose Row A and your opponent chooses Column D, you receive {M_on_screen[1]} and your opponent receives {M_on_screen[5]}.\n" +\
+    f"If you choose Row B and your opponent chooses Column C, you receive {M_on_screen[2]} and your opponent receives {M_on_screen[6]}.\n" +\
+    f"If you choose Row B and your opponent chooses Column D, you receive {M_on_screen[3]} and your opponent receives {M_on_screen[7]}.\n" +\
+    f"You press <<{'A' if up_choice==1 else 'B'}>>.\n"
 
-        all_prompts.append({'text': prompt, 'experiment': 'zhu2024games', 'participant': int(pId)})
+    all_prompts.append({'text': prompt, 'experiment': 'zhu2024games', 'participant': int(pId)})
         
 print(len(all_prompts))
 with jsonlines.open('prompts.jsonl', 'w') as writer:
