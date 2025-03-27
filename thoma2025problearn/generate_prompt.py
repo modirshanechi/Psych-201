1152c97b28000fd8ec423a355a68236a170c7c11
annaithoma <thoma@mpib-berlin.mpg.de>
2025-03-27 16:51:34 +0100

    included option name in description

--- thoma2025problearn/generate_prompt.py
+++ thoma2025problearn/generate_prompt.py
@@ -9,21 +9,6 @@ def randomized_choice_options(num_choices):
 all_prompts = []
 df = pd.read_csv('behavioral_data.csv')
 
-
-instr_dyn = "Welcome to the experiment! All animals have escaped from the zoo and are now hiding in the city. We need your help to find them. \n" \
-    "In this experiment you will repeatedly see these two houses and have to predict behind which house an animal will hide next. \n" \
-    "In each trial, an animal can hide behind one house, two houses or none. \n" \
-    "You can indicate your choice by tapping on one of the houses.\n" \
-    "After each choice, you will receive feedback about where the animal was hiding. You will receive a reward for each correct prediction.\n" \
-    "Please try to make as many correct choices as you can.\n" \
-
-instr_stat = "Welcome to the experiment! All animals have escaped from the zoo and are now hiding in the city. We need your help to find them. \n" \
-    "In this experiment you will repeatedly see these two houses and have to predict behind which house an animal will hide next. \n" \
-    "In each trial, an animal hides behind one of the houses. \n" \
-    "You can indicate your choice by tapping on one of the houses.\n" \
-    "After each choice, you will receive feedback about where the animal was hiding. You will receive a reward for each correct prediction.\n" \
-    "Please try to make as many correct choices as you can.\n" \
-
 num_trials = df.trial.max() 
 
 for participant in df['id'].unique():
@@ -32,6 +17,20 @@ for participant in df['id'].unique():
     cond = df_participant.cond.iloc[0]
     gender = df_participant.gender.iloc[0]
     choice_options = randomized_choice_options(2)
+
+    instr_dyn = f"Welcome to the experiment! All animals have escaped from the zoo and are now hiding in the city. We need your help to find them. \n" \
+        f"In this experiment you will repeatedly see two houses labeled '{choice_options[0]}' and {choice_options[1]}' and have to predict behind which house an animal will hide next. \n" \
+        "In each trial, an animal can hide behind one house, two houses or none. \n" \
+        f"You can indicate your choice by tapping on either house '{choice_options[0]}' or house '{choice_options[1]}'.\n" \
+        "After each choice, you will receive feedback about where the animal was hiding. You will receive a reward for each correct prediction.\n" \
+        "Please try to make as many correct choices as you can.\n"\
+
+    instr_stat = "Welcome to the experiment! All animals have escaped from the zoo and are now hiding in the city. We need your help to find them. \n" \
+        f"In this experiment you will repeatedly see two houses labeled '{choice_options[0]}' and {choice_options[1]}' and have to predict behind which house an animal will hide next. \n" \
+        "In each trial, an animal hides behind one of the houses. \n" \
+        f"You can indicate your choice by tapping on either house '{choice_options[0]}' or house '{choice_options[1]}'.\n" \
+        "After each choice, you will receive feedback about where the animal was hiding. You will receive a reward for each correct prediction.\n" \
+        "Please try to make as many correct choices as you can.\n" \
 
     if cond == "ecol_dyn": 
         prompt = instr_dyn
@@ -50,7 +49,7 @@ for participant in df['id'].unique():
         correct = df_trial.correct.iloc[0]
                         
         prompt += (
-            f"Trial {trial}: You pressed option <<{choice_options[button_pressed]}>>. {'At option ' + choice_options[0] + ', there was an animal hiding' if target_left == 1 else 'At option ' + choice_options[0] + ', there was no animal hiding'}. {'At option ' + choice_options[1] + ', there was an animal hiding' if target_right == 1 else 'At option ' + choice_options[1] + ', there was no animal hiding'}.\n"
+            f"Trial {trial}: You choose house <<{choice_options[button_pressed]}>>. {"At house '" + choice_options[0] + "', there is an animal hiding" if target_left == 1 else "At house '" + choice_options[0] + "', there is no animal hiding"}. {"At house '" + choice_options[1] + "', there is an animal hiding" if target_right == 1 else "At house '" + choice_options[1] + "', there is no animal hiding"}.\n"
             f"Your choice was {'correct and you receive a reward' if correct == 1 else 'incorrect and you do not receive a reward'}.\n"
         )
         
