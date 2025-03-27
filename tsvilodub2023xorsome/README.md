61c0dad8d87247c123ef8e5ce4c8b7f238418334
OLEKSII ARIASOV <oleksii.ariasov@student.uni-tuebingen.de>
2025-03-27 14:22:16 +0100

    Updated prompts for tsvilodub 2023 Xor paper

--- tsvilodub2023xorsome/README.md
+++ tsvilodub2023xorsome/README.md
@@ -1,3 +1,5 @@
+4 example trials were excluded, each participant had 76 main trials including test trials and attention checkers. Z-scores were provided as an additional metrics with Z-scores for test and attention check trials assigned to None. The prompts exceeded 32000 characters limit however on average for 10000 characters only.
+
 **Reference:**
 Tsvilodub, Polina & van Tiel, Bob & Franke, Michael. (2023). The role of relevance, competence, and priors for scalar inferences. Experiments in Linguistic Meaning.
 https://journals.linguisticsociety.org/proceedings/index.php/ELM/article/view/5375
--- tsvilodub2023xorsome/generate_prompts.py
+++ tsvilodub2023xorsome/generate_prompts.py
@@ -5,7 +5,8 @@ from tqdm import tqdm
 sys.path.append("")
 
 json_out = []
-CHARACTER_LIMIT = 32000
+CHARACTER_LIMIT = 50000
+
 
   ###Experiment
 
@@ -16,17 +17,25 @@ df_help = pd.read_csv("results_prereg_tidy_final_zScored_long.csv")
 # general task instructions
 task_instructions = (
     "Thank you for participating in this experiment!\n"
-    "You will have 76 trials of the same task.\n"
-    "Some of them will be attention checkers where you will be explicitly informed what to do.\n"
-    "In each trial you will read a short story and see a sentence in the blue box.\n"
-    "Then you will need to answer the question by adjusting the slider on the scale from 0 to 100, \n"
-    "Where 0 means that the sentence in the box is certainly false and 100 - certainly true.\n"
-    "Try to be as accurate as possible.\n\n"
+    "In the following, you will be presented with 16 short stories.\n"
+    "Please read them very carefully, even if they appear to be repeated and you think that you remember them well enough.\n"
+    "We ask you to rate statements about each short story.\n"
+    "Please indicate, using an adjustable slider, how likely it is that a statement is true based on what you've read.\n"
+    "Notice that there will also be simple attention checking trials.\n"
+    "You will recognize them immediately when you read the important text on each trial carefully - those trials contain instructions for you to move the slider in a certain way.\n"
+    "Please follow those instructions.\n"
+    "The story is given at the top.\n"
+    "The statement to be rated is given in a blue box below.\n"
+    "On some trials, you will see an additional sentence in a pink box.\n"
+    "Please read this sentence carefully before rating the statements in the boxes.\n\n"
+
 )
 
 #every trial instruction
 instructions_trial= (
+    "Please provide a rating of how likely it is that the statement is true, given the provided story.\n"
     "Story: {story}\n"
+    "{crit_question}"
     "Sentence in the blue box: \"{sentence}\"\n"
     "{question}\n"
     "Your answer: {response}.\n"
@@ -40,43 +49,50 @@ for participant in tqdm(df.submission_id.unique()):
     RT = []
     #reindex and drop the old index
     par_df = df[df.submission_id == participant].reset_index(drop=True)
-    # iterate over trials, construct interpretation and production instructions
+    # iterate over trials
     for _, trial in par_df.iterrows():
-        # retrieve the endpoints of the scale, response, name, reaction time and the adjective under consideration
+        # retrieve the response and the story
         response = trial["response"]
         story = trial["QUD"]
+
+        #retrieve the additional information if available
         if isinstance(trial["critical_question"], str):
             crit_quest = trial["critical_question"]
         else:
             crit_quest = ""
 
+        #retrive the prompt, the question, the condition, the reaction time and the trial number
         prompt = trial["prompt"]
         question = trial["question"]
         condition = trial["condition"]
         RT.append(trial["RT"])
         trial_number = trial["trial_number"]
 
+        #adjust the question
         font_inx = question.find("</font>")
         new_question = question[15:font_inx]
 
+        #adjust the prompt
         b_inx = prompt.find("</b>")
         prompt = prompt[3:b_inx]
 
+        #adjust the additional information if available
         if crit_quest:
             b_inx = crit_quest.find("</b>")
             crit_quest = crit_quest[3:b_inx]
+            crit_quest = "Sentence in the pink box: " + crit_quest+"\n"
 
-        story+=" " + crit_quest
-
+        # add z-scores
         if condition == "critical":
             z_scores = df_help.loc[(df_help['submission_id'] == participant) & (df_help['trial_number'] == trial_number), 'response_centered']
             z_scored_rating.append(z_scores.iloc[0])
         else:
-            z_scored_rating.append(0)
+            z_scored_rating.append(None)
 
         #fill the parameters to the trial outputs
         trial_instuction = instructions_trial.format(
             story=story,
+            crit_question=crit_quest,
             sentence = prompt,
             question = new_question,
             response=f"<<{response}>>"
