4c7263bbd915602289ba7fd0095ccc65a6b3f033
valentinkm <valentin.kriegmair@gmail.com>
2025-03-31 19:57:21 +0200

    update

--- anvari2024armed_bandit/generate_prompts.py
+++ anvari2024armed_bandit/generate_prompts.py
@@ -4,7 +4,7 @@ import pandas as pd
 import jsonlines
 
 
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
